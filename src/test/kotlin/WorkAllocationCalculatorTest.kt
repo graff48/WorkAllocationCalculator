2813150ae5127047d5ed394fee5596c2d879350f
Bob Graffagnino <graff-home@Bobs-Mac-mini.attlocal.net>
2021-09-19 16:09:44 -0400

    Adds default WorkAllocation to WorkAllocationCalculator

--- src/test/kotlin/WorkAllocationCalculatorTest.kt
+++ src/test/kotlin/WorkAllocationCalculatorTest.kt
@@ -9,7 +9,7 @@ class WorkAllocationCalculatorTest {
         Assert.assertNotNull(calculator)
     }
 
-    /**Given a collection of WorkAllocation(project planning, project work, administrative),  a date range, and a default WorkAllocation - calculate
+    /**Given a collection of WorkAllocation(agile ceremonies, story work, administrative),  a date range, and a default WorkAllocation - calculate
         the average work across the date range.  Use the default WorkAllocation for any missing or invalid WorkAllocation's
     */
     @Test
@@ -17,4 +17,12 @@ class WorkAllocationCalculatorTest {
         val workAllocation = WorkAllocation()
         Assert.assertNotNull(workAllocation)
     }
+
+    @Test
+    fun shouldCreateWorkAllocationCalculatorWithDefaultAllocation() {
+        val workAllocation = WorkAllocation()
+        val calculator = WorkAllocationCalculator(workAllocation)
+        Assert.assertNotNull(calculator)
+    }
+    
 }
