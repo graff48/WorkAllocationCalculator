b23ab7932f12537a5003a70eae2ae2312641e44c
Bob Graffagnino <graff-home@Bobs-Mac-mini.attlocal.net>
2021-09-19 16:14:10 -0400

    Implements WorkAllocationCalculator constructor with default WorkAllocation

--- src/main/kotlin/WorkAllocationCalculator.kt
+++ src/main/kotlin/WorkAllocationCalculator.kt
@@ -1,2 +1,6 @@
-class WorkAllocationCalculator {
+
+/**Given a collection of WorkAllocation(agile ceremonies, story work, administrative),  a date range, and a default WorkAllocation - calculate
+the average work across the date range.  Use the default WorkAllocation for any missing or invalid WorkAllocation's
+ */
+class WorkAllocationCalculator(val defaultWorkAllocation: WorkAllocation) {
 }
--- src/test/kotlin/WorkAllocationCalculatorTest.kt
+++ src/test/kotlin/WorkAllocationCalculatorTest.kt
@@ -4,15 +4,6 @@ import org.junit.Test
 class WorkAllocationCalculatorTest {
 
     @Test
-    fun shouldCreateWorkAllocationCalculator() {
-        val calculator = WorkAllocationCalculator()
-        Assert.assertNotNull(calculator)
-    }
-
-    /**Given a collection of WorkAllocation(agile ceremonies, story work, administrative),  a date range, and a default WorkAllocation - calculate
-        the average work across the date range.  Use the default WorkAllocation for any missing or invalid WorkAllocation's
-    */
-    @Test
     fun shouldCreateWorkAllocation() {
         val workAllocation = WorkAllocation()
         Assert.assertNotNull(workAllocation)
@@ -24,5 +15,9 @@ class WorkAllocationCalculatorTest {
         val calculator = WorkAllocationCalculator(workAllocation)
         Assert.assertNotNull(calculator)
     }
-    
+
+    /**Given a collection of WorkAllocation(agile ceremonies, story work, administrative),  a date range, and a default WorkAllocation - calculate
+    the average work across the date range.  Use the default WorkAllocation for any missing or invalid WorkAllocation's
+     */
+
 }
