79b8dbebee5a78223c870d8cea761527940f5e3d
Bob Graffagnino <graff-home@Bobs-Mac-mini.attlocal.net>
2021-09-19 14:35:05 -0400

    WorkAllocation test

--- src/test/kotlin/WorkAllocationCalculatorTest.kt
+++ src/test/kotlin/WorkAllocationCalculatorTest.kt
@@ -1,2 +1,20 @@
+import org.junit.Assert
+import org.junit.Test
+
 class WorkAllocationCalculatorTest {
+
+    @Test
+    fun shouldCreateWorkAllocationCalculator() {
+        val calculator = WorkAllocationCalculator()
+        Assert.assertNotNull(calculator)
+    }
+
+    /**Given a collection of WorkAllocation(project planning, project work, administrative),  a date range, and a default WorkAllocation - calculate
+        the average work across the date range.  Use the default WorkAllocation for any missing or invalid WorkAllocation's
+    */
+    @Test
+    fun shouldCreateWorkAllocation() {
+        val workAllocation = WorkAllocation()
+        Assert.assertNotNull(workAllocation)
+    }
 }
